7572b3db916c3e96f02c2096c67c0685546139be
mirelle casimiro <mirellecs07@gmail.com>
2025-06-03 18:56:32 -0300

    atualizando minhaFila e minhaFilaEncadeada

--- Roteiro TAD NEW/src/tad/fila/MinhaFila.java
+++ Roteiro TAD NEW/src/tad/fila/MinhaFila.java
@@ -9,7 +9,7 @@ package tad.fila;
 public class MinhaFila implements FilaIF<Integer> {
 	
 	private int tamanho = 10;
-	
+
 	private int cauda = 1;
 	private int cabeca = 0;
 	
@@ -17,40 +17,80 @@ public class MinhaFila implements FilaIF<Integer> {
 
 	public MinhaFila(int tamanhoInicial) {
 		tamanho = tamanhoInicial;
+		meusDados = new Integer[tamanho];
 	}
-	
+
 	public MinhaFila() {
+		meusDados = new Integer[tamanho];
 	}
 
 	@Override
-	public void enfileirar(Integer item) {
-		throw new UnsupportedOperationException("Implementar");
-		
+	public void enfileirar(Integer item) throws FilaCheiaException {
+		if (isFull()){
+			throw new FilaCheiaException();
+		}
+
+		meusDados[cauda] = item;
+		cauda = (cauda + 1) % tamanho;
 	}
 
 	@Override
-	public Integer desenfileirar() {
-		throw new UnsupportedOperationException("Implementar");
+	public Integer desenfileirar() throws FilaVaziaException {
+
+		if(isEmpty()){
+			throw new FilaVaziaException();
+		}
+
+		Integer item = meusDados[cauda];
+		meusDados[cabeca] = null;
+		cabeca = (cabeca + 1) % tamanho;
+		return item;
 	}
 
 	@Override
 	public Integer verificarCauda() {
-		throw new UnsupportedOperationException("Implementar");
+		if (isEmpty()) {
+			return null;
+		}
+
+		int posicao = (cauda - 1 + tamanho) % tamanho;
+		return meusDados[posicao];
 	}
 
 	@Override
 	public Integer verificarCabeca() {
-		throw new UnsupportedOperationException("Implementar");
+
+		if (isEmpty()) {
+			return null;
+		}
+		return meusDados[cabeca];
 	}
 
 	@Override
 	public boolean isEmpty() {
-		throw new UnsupportedOperationException("Implementar");
+		return cabeca == cauda && meusDados[cabeca] == null;
 	}
 
 	@Override
 	public boolean isFull() {
-		throw new UnsupportedOperationException("Implementar");
+		return cabeca == cauda && meusDados[cabeca] != null;
 	}
+
+	@Override
+	public int capacidade() {
+		return tamanho;
+	}
+
+	@Override
+	public int tamanho() {
+		int count = 0;
+		for (Integer item : meusDados) {
+			if (item != null) {
+				count++;
+			}
+		}
+		return count;
+	}
+
 
 }
--- Roteiro TAD NEW/src/tad/fila/MinhaFilaEncadeada.java
+++ Roteiro TAD NEW/src/tad/fila/MinhaFilaEncadeada.java
@@ -2,40 +2,98 @@ package tad.fila;
 
 public class MinhaFilaEncadeada implements FilaIF<Integer> {
 
+	//private ListaEncadeadaIF filaEncadeada = null;
+
+	private class Node {
+		Integer valor;
+		Node prox;
+
+		Node(Integer valor) {
+			this.valor = valor;
+			this.prox = null;
+		}
+	}
+
+	private Node cabeca = null;
+	private Node cauda = null;
+	private int tamanho = 0;
+
 	@Override
 	public void enfileirar(Integer item) throws FilaCheiaException {
 		// TODO Auto-generated method stub
-		
+
+		Node novo = new Node(item);
+		if(isEmpty()){
+			cabeca = novo;
+			cauda = novo;
+		}
+		else {
+			//cabeca.prox = novo;
+			cauda.prox = novo;
+			cauda = novo;
+		}
+		tamanho++;
 	}
 
 	@Override
 	public Integer desenfileirar() throws FilaVaziaException {
 		// TODO Auto-generated method stub
-		return null;
+
+		if(isEmpty()){
+			throw new FilaVaziaException();
+		}
+
+		Integer valor = cabeca.valor;
+		cabeca = cabeca.prox;
+
+		if(cabeca == null){
+			cauda = null;
+		}
+		tamanho--;
+		return valor;
 	}
 
 	@Override
 	public Integer verificarCauda() {
 		// TODO Auto-generated method stub
+
+		if(cauda != null){
+			return cauda.valor;
+		}
+
 		return null;
 	}
 
 	@Override
 	public Integer verificarCabeca() {
 		// TODO Auto-generated method stub
+
+		if(cabeca!=null){
+			return cabeca.valor;
+		}
 		return null;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return tamanho == 0;
 	}
 
 	@Override
 	public boolean isFull() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public int capacidade() {
+		return Integer.MAX_VALUE;
+	}
+
+	@Override
+	public int tamanho() {
+		return tamanho;
 	}
 
 }
